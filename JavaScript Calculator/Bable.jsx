7787c5f8df58ee9582883403b3913f0097c2b3db
ashwinangadi <ashwin.angadi1@gmail.com>
2023-03-30 23:41:08 +0530

    Update Bable.jsx

--- JavaScript Calculator/Bable.jsx
+++ JavaScript Calculator/Bable.jsx
@@ -1,40 +1,234 @@
+// switch (input) {
+//   case "/":
+//     if (
+//       ["/","x"].includes(this.state.display[this.state.display.length - 2]) &&
+//       ["-"].includes(this.state.display[this.state.display.length - 1])
+//     ) {
+//       console.log("ghgg")
+//       this.setState({
+//         display:
+//           this.state.display.substring(0, this.state.display.length - 2) +
+//           input
+//       });
+//     } else if (
+//       ["/", "x", "+", "-"].includes(
+//         this.state.display[this.state.display.length - 1]
+//       )
+//     ) {
+//       this.setState({
+//         display:
+//           this.state.display.substring(0, this.state.display.length - 1) +
+//           input
+//       });
+//     } /*else {
+//       this.setState({
+//         display:
+//           this.state.display.substring(0, this.state.display.length - 1) +
+//           input
+//       });
+//     }*/
+//     break;
+//   case "x":
+//     if (
+//       ["/", "x"].includes(this.state.display[this.state.display.length - 2]) &&
+//       ["-"].includes(this.state.display[this.state.display.length - 1])
+//     ) {
+//       this.setState({
+//         display:
+//           this.state.display.substring(0, this.state.display.length - 2) +
+//           input
+//       });
+//     } else if (
+//       ["x", "/", "+"].includes(
+//         this.state.display[this.state.display.length - 1]
+//       )
+//     ) {
+//       this.setState({
+//         display:
+//           this.state.display.substring(0, this.state.display.length - 1) +
+//           input
+//       });
+//     }
+//     break;
+//   case "+":
+//     if (
+//       ["x", "/", "+"].includes(
+//         this.state.display[this.state.display.length - 1]
+//       )
+//     ) {
+//       this.setState({
+//         display:
+//           this.state.display.substring(0, this.state.display.length - 1) +
+//           input
+//       });
+//     }
+//     break;
+//   case "-":
+//     if (
+//       ["-"].includes(
+//         this.state.display[this.state.display.length - 1]
+//       )
+//     ) {
+//       this.setState({
+//         display:
+//           this.state.display.substring(0, this.state.display.length - 1) +
+//           input
+//       });
+//     }
+//     break;
+// }
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      expression: "",
       display: "0"
     };
     this.clicked = this.clicked.bind(this);
+    this.operatorInput = this.operatorInput.bind(this);
+    this.numberInput = this.numberInput.bind(this);
+    this.calculateExp = this.calculateExp.bind(this);
   }
 
   clicked = (event) => {
     const { display } = this.state;
     const { outerText } = event.target;
 
-    // switch (outerText) {
-    //   case Number(outerText):
-    //     numberInput();
-    //     break;
-    // }
-
-    this.setState({
-      display: display + outerText
-    });
-
-    //To reject first input as operator
-    if (["+", "x", "/"].includes(display[1])) {
-      this.setState({
-        display: display.slice(1) + outerText
-      });
-    }
-
-    //To clear display
-    if (outerText == "AC") {
-      this.setState({
-        display: "0"
-      });
+    switch (true) {
+      case ["/", "x", "+", "-"].includes(outerText):
+        this.operatorInput(outerText);
+        break;
+      case !isNaN(outerText) || outerText == ".":
+        console.log("number case");
+        this.numberInput(outerText);
+        break;
+      case outerText === "=":
+        this.setState({
+          display: this.calculateExp(this.state.expression)
+        });
+      case outerText === "AC":
+        this.setState({
+          display: "0"
+        });
+        break;
     }
   };
+  operatorInput(input) {
+    console.log("-----operator input---------");
+    if (this.state.display === "0") {
+      console.log("in 0", input);
+      this.setState({
+        display: input
+      });
+    } else {
+      switch (true) {
+        case ["+", "-", "/", "x", "."].includes(
+          this.state.display[this.state.display.length - 1]
+        ):
+          if (
+            input === "-" &&
+            ["/", "x"].includes(
+              this.state.display[this.state.display.length - 1]
+            ) &&
+            this.state.display.length !== 1
+          ) {
+            this.setState({
+              display: this.state.display + input
+            });
+          } else {
+            if (
+              this.state.display[this.state.display.length - 1] === "-" &&
+              ["/", "x"].includes(
+                this.state.display[this.state.display.length - 2]
+              )
+            ) {
+              this.setState({
+                display:
+                  this.state.display.substring(
+                    0,
+                    this.state.display.length - 2
+                  ) + input
+              });
+            } else {
+              this.setState({
+                display:
+                  this.state.display.substring(
+                    0,
+                    this.state.display.length - 1
+                  ) + input
+              });
+            }
+          }
+
+          break;
+        default:
+          this.setState({
+            display: this.state.display + input
+          });
+      }
+    }
+  }
+  numberInput(input) {
+    console.log("-----number input---------");
+    if (input === ".") {
+      let exp = this.state.display.split(/[+\-/X]/g);
+      console.log("in decimal exp", exp);
+      if (this.state.display === "0") {
+        this.setState({
+          display: "0" + input
+        });
+      } else if (
+        ["+", "-", "/", "x"].includes(
+          this.state.display[this.state.display.length - 1]
+        )
+      ) {
+        if (this.state.display.length === 1) {
+          this.setState({
+            display: "0" + input
+          });
+        } else {
+          this.setState({
+            display: this.state.display + "0" + input
+          });
+        }
+      } else if (this.state.display[this.state.display.length - 1] === ".") {
+        console.log("you fool, you cant add two decimals");
+      } else if (exp[exp.length - 1].includes(".")) {
+        console.log("you fool, you cant add two decimals");
+      } else {
+        this.setState({
+          display: this.state.display + input
+        });
+      }
+    } else {
+      if (this.state.display === "0") {
+        console.log("in 0", input);
+        this.setState({
+          display: input
+        });
+      } else if (
+        ["/", "x", "+"].includes(
+          this.state.display[this.state.display.length - 1]
+        ) &&
+        this.state.display.length === 1
+      ) {
+        this.setState({
+          display:
+            this.state.display.substring(0, this.state.display.length - 1) +
+            input
+        });
+      } else {
+        console.log("in numInput last else");
+        this.setState({
+          display: this.state.display + input
+        });
+      }
+    }
+  }
+  calculateExp(input) {
+    console.log("-----evalute = input---------");
+  }
 
   render() {
     const { display } = this.state;
@@ -53,10 +247,8 @@ class App extends React.Component {
           id="display"
           className="container p-0 d-flex justify-content-end align-items-end text-light fs-3"
         >
-          {display.length == 1 && display == "0"
-            ? display
-            : display.substring(1)}
-          {/*console.log(display)*/}
+          {display}
+          {console.log(display)}
         </div>
         <div
           id="keypad"
